45803043b8dbc0d59c572462d9ebc128c50849aa
Ali Muhammad <ali309muhammed@gmail.com>
2023-10-14 06:08:17 +0500

    backend Model and controllers added

---
+++ controllers/openaiController.js
@@ -0,0 +1,60 @@
+const OPENAI = require("openai").OpenAI;
+
+const openai = new OPENAI();
+
+
+
+
+exports.paraphrasing = async (req, res) => {
+    try {
+      const { prompt } = req.body;
+      console.log(prompt);
+      const response = await openai.completions.create({
+        model: "gpt-3.5-turbo-instruct",
+        prompt: `Give me best suggested paragraph and paraphrase it:"${prompt}"`,
+        max_tokens: 1000,
+        temperature: 1.5,
+        n: 3,
+      });
+      return res.status(200).json({
+        success: true,
+        data: response.choices,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        success: false,
+        error: error.response
+          ? error.response.data
+          : "there was an issue on server",
+      });
+    }
+  }
+
+
+
+// Other OpenAI-related route handlers
+
+exports.continueWriting = async (req, res) => {
+    try {
+      const { prompt } = req.body;
+      console.log(prompt, "working with contine");
+      const response = await openai.completions.create({
+        model: "gpt-3.5-turbo-instruct",
+        prompt: `${prompt}continue writing provide me atleast two paragraph `,
+        max_tokens: 1000,
+      });
+  
+      console.log(response.choices[0].text);
+      return res.status(200).json({
+        success: true,
+        data: response.choices[0].text,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        success: false,
+        error: error.response
+          ? error.response.data
+          : "there was an issue on server",
+      });
+    }
+  }
